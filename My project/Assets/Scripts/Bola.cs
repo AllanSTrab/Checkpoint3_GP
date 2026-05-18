38df113ea0fa0725ee2359d64cc138c0159500e8
AllanSTrab <pekagamestrabbold@gmail.com>
2026-05-18 10:33:56 -0300

    CP03Terminado

--- My project/Assets/Scripts/Bola.cs
+++ My project/Assets/Scripts/Bola.cs
@@ -4,31 +4,68 @@ public class Bola : MonoBehaviour
 {
     int score01 = 0;
     int score02 = 0;
-
+    float bolamovy = 1f;
+    float bolamovx = 0f;
     private void Start()
     {
-        transform.Translate(0f, -1f * Time.deltaTime * 5f, 0f);
+
     }
     void Update()
     {
+        if(score01 > 4 || score02 > 4)
+        {
+            return;
+        }
         if (transform.position.y < -5f)
         {
             print("Ponto para o jogador 02");
+            print("Jogador 1: " + score01 + " pontos | Jogador 2: " + score02 + " pontos");
             score02++;
             transform.position = new Vector2(0f, 0f);
+            bolamovy = Random.Range(-1, 1);
+            if(bolamovy == 0f)
+            {
+                bolamovy = 1f;
+            }
         }
         else if (transform.position.y > 5f)
         {
             print("Ponto para o jogador 01");
+            print("Jogador 1: " + score01 + " pontos | Jogador 2: " + score02 + " pontos");
             score01++;
             transform.position = new Vector2(0f, 0f);
+            bolamovy = Random.Range(-1, 1);
+            if (bolamovy == 0f)
+            {
+                bolamovy = 1f;
+            }
         }
+        if (score01 > 4)
+        {
+            print("--------------------------");
+            print("Jogador 01 venceu!");
+            print("Pontuação Final:");
+            print("Jogador 1: " + score01 + " pontos | Jogador 2: " + score02 + " pontos");
+        }
+        else if (score02 > 4)
+        {
+            print("--------------------------");
+            print("Jogador 02 venceu!");
+            print("Pontuação Final:");
+            print("Jogador 1: " + score01 + " pontos | Jogador 2: " + score02 + " pontos");
+        }
+        transform.Translate(bolamovx * Time.deltaTime * 2f, bolamovy * Time.deltaTime * 7f, 0f);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player01" || collision.gameObject.tag == "Player02")
         {
-            transform.Translate(0f, -1f * Time.deltaTime * 5f, 0f);
+            bolamovy = bolamovy * -1f;
+            bolamovx = Random.Range(-2f, 2f);
+        }
+        if(collision.gameObject.tag == "Parede")
+        {
+            bolamovx = bolamovx * -1f;
         }
     }
 }
--- My project/Assets/Scripts/Player02.cs
+++ My project/Assets/Scripts/Player02.cs
@@ -1,28 +1,19 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player02 : MonoBehaviour
 {
     int score01 = 0;
     int score02 = 0;
     float speed = 10f;
     bool podeMover = true;
-    void Start()
-    {
-        if(gameObject.tag == "Player01")
-        {
-            print("Jogador 01 use A e D para se mover");
-            print("Jogador 02 use as setas para se mover");
-        }
-    }
-
     void Update()
     {
-        if(score01 > 4 || score02 > 4)
+        if (score01 > 4 || score02 > 4)
         {
             podeMover = false;
             return;
         }
-        if(gameObject.tag == "Bola")
+        if (gameObject.tag == "Bola")
         {
             if (transform.position.y < -5f)
             {
@@ -35,18 +26,17 @@ public class Player : MonoBehaviour
         }
         if(podeMover == true)
         {
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.LeftArrow))
             {
                 transform.Translate((-1f * Time.deltaTime * speed), 0f, 0f);
             }
-            else if (Input.GetKey(KeyCode.D))
+            else if (Input.GetKey(KeyCode.RightArrow))
             {
                 transform.Translate((1f * Time.deltaTime * speed), 0f, 0f);
             }
             if (score01 > 4 || score02 > 4)
             {
-                transform.position = new Vector2(0f, -4f);
-
+                transform.position = new Vector2(0f, 4f);
             }
         }
     }
